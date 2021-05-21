6233aa3e5eb37a67c4fa975511032015440ab6d7
nmar30 <nmarcisso@live.com>
2021-05-21 10:58:49 -0400

    Added styling in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MusicTable from './components/MusicTable/MusicTable'
 
 class App extends Component {
     constructor(props){
@@ -18,13 +19,40 @@ class App extends Component {
         this.setState({
             songs: response.data
         })
-        console.log(this.songs)
+    }
+
+    async deleteSong(props) {
+        let response = await axios.delete('http://127.0.0.1:8000/music/'[props.song.id])
+        this.getSongs()
+    }
+
+    renderTable = () => {
+        return (
+            this.state.songs.map((song) => 
+            <MusicTable song={song} deleteSong={this.deleteSong}/>
+            )
+        )
     }
 
     render() { 
         return (
-        <div>
-
+        <div className="container">
+            <h1>Music Library</h1>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                    <th>Title</th>
+                    <th>Album</th>
+                    <th>Artist</th>
+                    <th>Genre</th>
+                    <th>Release Date</th>
+                    <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderTable()}
+                </tbody>
+            </table>
         </div>
         );
     }
---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MusicTable(props){
+    return (
+        <tr>
+            <td>{props.song.title}</td>
+            <td>{props.song.album}</td>
+            <td>{props.song.artist}</td>
+            <td>{props.song.genre}</td>
+            <td>{props.song.release_date}</td>
+            <td><button type="button" className="btn btn-danger" onClick={() => props.deleteSong([props.song.id])}>Delete</button></td>
+        </tr>
+    )
+}
+
+ 
+export default MusicTable;
